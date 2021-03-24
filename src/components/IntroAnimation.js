d8aa0da1ef49fb3b0d5f9863dbd3235023378a90
Katerina Vaseva <katerina.vsv@gmail.com>
2021-03-24 13:38:40 +0200

    add loader and instructions

--- src/components/IntroAnimation.js
+++ src/components/IntroAnimation.js
@@ -7,17 +7,91 @@ import crocs from "@assets/crocs.png";
 import MotionPathPlugin from "@plugins/MotionPathPlugin";
 gsap.registerPlugin(MotionPathPlugin);
 
-const Hand = forwardRef((props, ref) => <img src={open} ref={ref} />);
-const Jibbitz = forwardRef((props, ref) => <img src={crocs} ref={ref} />);
+const bodyScrollLock = require("body-scroll-lock");
+const disableBodyScroll = bodyScrollLock.disableBodyScroll;
+const enableBodyScroll = bodyScrollLock.enableBodyScroll;
+
+const Hand = forwardRef((props, ref) => (
+  <img
+    css={css`
+      position: absolute;
+      transform: translate3d(-50%, -50%, 0);
+      top: 16%;
+      left: 42%;
+      opacity: 0;
+    `}
+    src={open}
+    ref={ref}
+  />
+));
+const Jibbitz = forwardRef((props, ref) => (
+  <img
+    css={css`
+      max-width: 180px;
+      position: absolute;
+      top: 0;
+      left: 18%;
+      transform: translate3d(-50%, -50%, 0);
+      opacity: 0;
+    `}
+    src={crocs}
+    ref={ref}
+  />
+));
 
 const IntroAnimation = () => {
+  const loaderRef = useRef(null);
   const handRef = useRef(null);
   const jibbitzRef = useRef(null);
+  const handPathRef = useRef(null);
+  const dragPathRef = useRef(null);
 
-  console.log(MorphSVGPlugin);
+  useEffect(() => {
+    disableBodyScroll(document.querySelector("body"));
+    const tl = gsap
+      .timeline()
+      .to([handRef.current, jibbitzRef.current], {
+        opacity: 1,
+        duration: 0.15,
+      })
+      .to(handRef.current, {
+        motionPath: {
+          path: handPathRef.current,
+          align: handPathRef.current,
+          alignOrigin: [0.5, 0.5],
+          autoRotate: false,
+        },
+        transformOrigin: "50% 50%",
+        delay: 0.5,
+        duration: 1,
+        ease: "power1.inOut",
+      })
+      .to([handRef.current, jibbitzRef.current], {
+        motionPath: {
+          path: dragPathRef.current,
+          align: dragPathRef.current,
+          alignOrigin: [0.5, 0.5],
+          autoRotate: false,
+        },
+        transformOrigin: "50% 50%",
+        duration: 2,
+        delay: 0.5,
+        ease: "power1.inOut",
+      })
+      .to(loaderRef.current, {
+        opacity: 0,
+        pointerEvents: "none",
+        duration: 1,
+        delay: 1,
+        onComplete: () => {
+          enableBodyScroll(document.querySelector("body"));
+        },
+      });
+  }, []);
 
   return (
     <div
+      ref={loaderRef}
       css={css`
         position: fixed;
         top: 0;
@@ -33,11 +107,83 @@ const IntroAnimation = () => {
     >
       <div
         css={css`
-          max-width: 800px;
+          max-width: 600px;
           padding: 0px 16px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-direction: column;
         `}
       >
-        <H4>Click and drag the Jibbitz to move them around!</H4>
+        <h6
+          css={css`
+            @keyframes blink-animation {
+              to {
+                visibility: hidden;
+              }
+            }
+            @-webkit-keyframes blink-animation {
+              to {
+                visibility: hidden;
+              }
+            }
+            animation: blink-animation 1s steps(5, start) infinite;
+            -webkit-animation: blink-animation 1s steps(5, start) infinite;
+            font-family: "univers-pro", sans-serif;
+            font-size: 24px;
+            text-transform: uppercase;
+            margin-bottom: 30px;
+          `}
+        >
+          (loading)
+        </h6>
+        <H4
+          css={css`
+            font-family: "monument";
+          `}
+        >
+          Click and drag the Jibbitz to move them around!
+        </H4>
+      </div>
+      <div
+        css={css`
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          top: 0;
+          left: 0;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          pointer-events: none;
+
+          path {
+            fill: none;
+          }
+        `}
+      >
+        <div
+          css={css`
+            max-width: 80vw;
+            position: relative;
+            width: 100%;
+          `}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 912.91 511.1">
+            <path
+              ref={handPathRef}
+              d="M740.5,277.5s-168,96-212-87"
+              transform="translate(-354.04 -190)"
+            />
+            <path
+              ref={dragPathRef}
+              d="M528.5,190.5s-162,8-173,83c-13,88.79,112,70,52,147"
+              transform="translate(-354.04 -190)"
+            />
+          </svg>
+          <Jibbitz ref={jibbitzRef} />
+          <Hand ref={handRef} />
+        </div>
       </div>
     </div>
   );
