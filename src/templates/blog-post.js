d5ce8745e486838a55bbd0b6b802e129466a2e40
katerinavaseva <katerina.vsv@gmail.com>
2022-10-19 09:00:21 +0200

    setup queries

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,73 @@
 import React from "react";
+import { graphql } from "gatsby";
 
 const BlogPost = ({ data }) => {
+  console.log(data);
   return <h1>blog post</h1>;
 };
 
 export default BlogPost;
+
+export const query = graphql`
+  query BlogPostQuery($slug: String!, $nextSlug: String!, $prevSlug: String!) {
+    nextPost: datoCmsBlogPost(pageSlug: { eq: $nextSlug }) {
+      pageSlug
+      title
+      featuredImage {
+        fluid {
+          src
+          srcSet
+          aspectRatio
+        }
+      }
+    }
+    prevPost: datoCmsBlogPost(pageSlug: { eq: $prevSlug }) {
+      pageSlug
+      title
+      featuredImage {
+        fluid {
+          src
+          srcSet
+          aspectRatio
+        }
+      }
+    }
+    post: datoCmsBlogPost(pageSlug: { eq: $slug }) {
+      title
+      metaTags {
+        description
+        image {
+          fluid {
+            src
+            srcSet
+          }
+        }
+      }
+      featuredImage {
+        fluid {
+          src
+          srcSet
+          aspectRatio
+        }
+      }
+      content {
+        ... on DatoCmsImagesBlock {
+          id
+          __typename
+          items {
+            fluid {
+              src
+              srcSet
+              aspectRatio
+            }
+          }
+        }
+        ... on DatoCmsTextBlock {
+          id
+          copy
+          __typename
+        }
+      }
+    }
+  }
+`;
--- src/templates/page.js
+++ src/templates/page.js
@@ -1,7 +1,51 @@
 import React from "react";
+import { graphql } from "gatsby";
 
 const Page = ({ data }) => {
+  console.log(data);
   return <h1>page</h1>;
 };
 
 export default Page;
+
+export const query = graphql`
+  query PageQuery($slug: String!) {
+    page: datoCmsPage(pageSlug: { eq: $slug }) {
+      title
+      metaTags {
+        description
+        image {
+          fluid {
+            src
+            srcSet
+          }
+        }
+      }
+      featuredImage {
+        fluid {
+          src
+          srcSet
+          aspectRatio
+        }
+      }
+      content {
+        ... on DatoCmsImagesBlock {
+          id
+          __typename
+          items {
+            fluid {
+              src
+              srcSet
+              aspectRatio
+            }
+          }
+        }
+        ... on DatoCmsTextBlock {
+          id
+          copy
+          __typename
+        }
+      }
+    }
+  }
+`;
